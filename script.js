0c81a5924ef9da4b7ff628c4455fc74faf82c7c6
Koh Tze Yong <tzeyong@gmail.com>
2020-09-22 01:11:20 +0800

    done

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 function compute() {
     p = document.getElementById("principal").value;
-    alert(p);
+    r = document.getElementById("rate").value;
+    y = document.getElementById("years").value;
+    var interest = p * r * y / 100;
+    var total = +interest + +p;
+    var d = new Date();
+    var year = d.getFullYear();
+    var derivedyear = +year + +y;
+
+    var resultText = "If you deposit " + p + ",<br>"
+        + "at an interest rate of " + r + "%.<br>"
+        + "You will receive an amount of " + total + ",<br>"
+        + "in the year " + derivedyear;
+
+    document.getElementById("result").innerHTML = resultText;
+}
+
+function updateRange() {
+    r = document.getElementById("rate").value;
+    document.getElementById("rangeSpan").textContent = r + "%";
 }
